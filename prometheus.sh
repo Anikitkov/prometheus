15ebfe7c41fe501d17bfbc454bec2bec78c9d434
Anikitkov <168273830+Anikitkov@users.noreply.github.com>
2024-09-11 19:46:38 +0300

    Update prometheus.sh
    i`m so tired

--- prometheus.sh
+++ prometheus.sh
@@ -39,13 +39,80 @@ Restart=on-failure
 WantedBy=multi-user.target
 EOF
 promtool check config /etc/prometheus/prometheus.yml
-systemctl enable prometheus
-systemctl start prometheus
-                          
+  
 #install alertmanager
+sudo mkdir -p /etc/alertmanager /var/lib/prometheus/alertmanager
+sudo cp /tmp/alertmanager*/alertmanager /usr/local/bin/
+sudo cp /tmp/alertmanager*/amtool /usr/local/bin/
+sudo cp /tmp/alertmanager*/alertmanager.yml  /etc/alertmanager/
+sudo useradd --no-create-home --shell /bin/false alertmanager
+sudo chown -R alertmanager:alertmanager /etc/alertmanager /var/lib/prometheus/alertmanager
+sudo chown alertmanager:alertmanager /usr/local/bin/{alertmanager,amtool}
+sudo touch /etc/systemd/system/alertmanager.service
+sudo chmod 777 /etc/systemd/system/alertmanager.service
+sudo cat > /etc/systemd/system/alertmanager.service <<EOF
+^[[200~[Unit]
+Description=Alertmanager Service
+After=network.target
+
+[Service]
+EnvironmentFile=-/etc/default/alertmanager
+User=alertmanager
+Group=alertmanager
+Type=simple
+ExecStart=/usr/local/bin/alertmanager           --config.file=/etc/alertmanager/alertmanager.yml           --storage.path=/var/lib/prometheus/alertmanager           --cluster.advertise-address=0.0.0.0:9093           $ALERTMANAGER_OPTS
+ExecReload=/bin/kill -HUP $MAINPID
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl enable alertmanager
+sudo systemctl start alertmanager
+
+#install node-exporter
 
 
 
+                          
+#install node-exporter
+sudo cp /tmp/node_exporter*/node_exporter /usr/local/bin/
+sudo useradd --no-create-home --shell /bin/false nodeexporter
+sudo chown -R nodeexporter:nodeexporter /usr/local/bin/node_exporter
+sudo touch /etc/systemd/system/node_exporter.service
+sudo chmod 777 /etc/systemd/system/node_exporter.service
+sudo cat > /etc/systemd/system/node_exporter.service <<EOF
+> [Unit]
+Description=Node Exporter Service
+After=network.target
+
+[Service]
+User=nodeexporter
+Group=nodeexporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+ExecReload=/bin/kill -HUP $MAINPID
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo cat >> /etc/prometheus/prometheus.yml <<EOF
+scrape_configs:
+  ...
+  - job_name: 'node_exporter_clients'
+    scrape_interval: 5s
+    static_configs:
+      - targets:
+          - 127.0.0.1:9100
+EOF
+
+sudo systemctl enable node_exporter
+sudo systemctl start node_exporter
+
+
+sudo systemctl enable prometheus
+sudo systemctl start prometheus
 
 
 
